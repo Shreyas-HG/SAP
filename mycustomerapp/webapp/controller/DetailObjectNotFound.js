566612cb15b89c6288f73b27ddb19bcddf90a11d
Shreyas-HG <shreyas.hg123@gmail.com>
2019-09-03 14:30:02 +0000

    2nd Commit

---
+++ mycustomerapp/webapp/controller/DetailObjectNotFound.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"./BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("demo.customer.mycustomerapp.controller.DetailObjectNotFound", {});
+});
